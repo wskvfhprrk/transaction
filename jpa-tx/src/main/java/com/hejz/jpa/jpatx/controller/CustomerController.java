5c482f70b34901a1f853013230e2ae5092c0f5bf
hejz <75412985@qq.com>
2022-11-04 16:18:36 +0800

    测试jpa事务-代码测试和注解测试

--- jpa-tx/src/main/java/com/hejz/jpa/jpatx/controller/CustomerController.java
+++ jpa-tx/src/main/java/com/hejz/jpa/jpatx/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.hejz.jpa.jpatx.controller;
 
 import com.hejz.jpa.jpatx.domain.Customer;
 import com.hejz.jpa.jpatx.service.CustomerServeAnnotation;
+import com.hejz.jpa.jpatx.service.CustomerServeCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +18,15 @@ import java.util.List;
 public class CustomerController {
     @Autowired
     private CustomerServeAnnotation customerServeAnnotation;
+    @Autowired
+    private CustomerServeCode customerServeCode;
     @PostMapping("saveByAnnotation")
     public Customer saveByAnnotation(@RequestBody Customer customer){
         return customerServeAnnotation.save(customer);
+    }
+    @PostMapping("saveByCode")
+    public Customer saveByCode(@RequestBody Customer customer){
+        return customerServeCode.save(customer);
     }
     @GetMapping("all")
     public List<Customer> getAll(){
---
+++ jpa-tx/src/main/java/com/hejz/jpa/jpatx/service/CustomerServeCode.java
@@ -0,0 +1,44 @@
+package com.hejz.jpa.jpatx.service;
+
+import com.hejz.jpa.jpatx.dao.CustomerRepositocy;
+import com.hejz.jpa.jpatx.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.util.List;
+
+/**
+ * @author:hejz 75412985@qq.com
+ * @create: 2022-11-04 13:26
+ * @Description: 标签实现事务
+ */
+@Service
+public class CustomerServeCode {
+
+    @Autowired
+    private CustomerRepositocy customerRepositocy;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    public Customer save(Customer customer){
+        DefaultTransactionDefinition dtf=new DefaultTransactionDefinition();
+        dtf.setIsolationLevel(TransactionDefinition.ISOLATION_DEFAULT);
+        dtf.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus ts = transactionManager.getTransaction(dtf);
+        try{
+            Customer customer1 = customerRepositocy.save(customer);
+            transactionManager.commit(ts);
+            return customer1;
+        }catch (Exception e){
+            transactionManager.rollback(ts);
+            throw e;
+        }
+
+    }
+
+}
