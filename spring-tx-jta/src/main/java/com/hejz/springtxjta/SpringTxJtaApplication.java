5a73abe86197be5a09eba3530e451a644de82026
hejz <75412985@qq.com>
2022-11-04 23:05:34 +0800

    jta事务管理

---
+++ spring-tx-jta/src/main/java/com/hejz/springtxjta/SpringTxJtaApplication.java
@@ -0,0 +1,13 @@
+package com.hejz.springtxjta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxJtaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTxJtaApplication.class, args);
+    }
+
+}
---
+++ spring-tx-jta/src/test/java/com/hejz/springtxjta/SpringTxJtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hejz.springtxjta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxJtaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
