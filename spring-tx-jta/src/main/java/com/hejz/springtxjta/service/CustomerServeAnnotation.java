c5c8d50349df19c2e8c12f16f19460e8a60b984e
hejz <75412985@qq.com>
2022-11-11 16:05:06 +0800

    jms双数据源事务测试

--- spring-tx-jta/src/main/java/com/hejz/springtxjta/service/CustomerServeAnnotation.java
+++ spring-tx-jta/src/main/java/com/hejz/springtxjta/service/CustomerServeAnnotation.java
@@ -2,7 +2,10 @@ package com.hejz.springtxjta.service;
 
 import com.hejz.springtxjta.dao.CustomerRepositocy;
 import com.hejz.springtxjta.domain.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,15 +17,25 @@ import java.util.List;
  * @Description: 注解实现事务
  */
 @Service
+@Slf4j
 public class CustomerServeAnnotation {
 
     @Autowired
     private CustomerRepositocy customerRepositocy;
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
     @Transactional
     public Customer save(Customer customer){
         error();
+        log.info("save:{}",customer);
+        jmsTemplate.convertAndSend("create:new",customer.getUsername());
         return customerRepositocy.save(customer);
+    }
+
+    @JmsListener(destination = "create:new")
+    public void listener(String name){
+        log.info("接收到消息：{}",name);
     }
 
     private void error() {
